64d5c41bcf3e9629606f3545114d811a15c04f88
JoaoCarvalho2002 <jonitkick@gmail.com>
2023-04-21 20:27:50 +0100

    Exercise 8 first writeline

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -25,7 +25,8 @@ namespace GameSix
                 instance.GetShield();
             
                 
-            }   
+            }  
+            Console.WriteLine("Obrigado por ter usado este programa!"); 
         }
         
     }
